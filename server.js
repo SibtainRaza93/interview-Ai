a167c8a91811db78384555046bcbd33482ef4228
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-18 19:00:47 +0530

    started Ai implementation

--- server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const connectToDB = require("./src/config/db.js")
-const app = require("./src/app.js")
-
-connectToDB()
-app.listen(3000, ()=>{
-    console.log("server is listening on port 3000");
-    
-})
